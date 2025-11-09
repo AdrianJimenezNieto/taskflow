4c91c79722e16287c5830748f427eab0d4956277
adrianJ <123407879+AdrianJimenezNieto@users.noreply.github.com>
2025-11-09 17:01:09 +0100

    feat(auth): US-102 - Add JwtTokenProvider and CustomUserDetailsService

---
+++ taskflow-api/src/main/java/com/taskflow/infrastructure/adapter/out/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,51 @@
+package com.taskflow.infrastructure.adapter.out.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+  // We include the values from application.propperties
+  @Value("${jwt.secret-key}")
+  private String secretKeyString;
+
+  @Value("{$jwt.expiration}")
+  private long jwtExpiration;
+
+  // Method to generate the token
+  public String generateToken(Authentication authentication) {
+    // Get the main user (UserDetails)
+    UserDetails mainUser = (UserDetails) authentication.getPrincipal();
+
+    String username = mainUser.getUsername();
+    Date now = new Date();
+    Date expiryDate = new Date(now.getTime() + jwtExpiration);
+
+    // Build the token
+    return Jwts.builder()
+            .setSubject(username) // Token owner (email)
+            .setIssuedAt(now)
+            .setExpiration(expiryDate)
+            .signWith(getSigningKey(), SignatureAlgorithm.HS512) // Sign the token
+            .compact();
+  }
+
+  // Private method to obtain the key
+  private SecretKey getSigningKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKeyString);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+
+  // TODO: Add the validation methods here
+  
+}
---
+++ taskflow-api/src/main/java/com/taskflow/infrastructure/adapter/out/security/jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.taskflow.infrastructure.adapter.out.security.jwt.service;
+
+import com.taskflow.domain.port.out.UserRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections; // For the roles
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+  // We depend on the domain port, as always
+  private final UserRepositoryPort userRepositoryPort;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    // We use the email bc our username can be doubled
+
+    // We search the user on our Database using the port
+    com.taskflow.domain.model.User ourUser = userRepositoryPort.findByEmail(email)
+      .orElseThrow(() ->
+        new UsernameNotFoundException("Usuario no encontrado con el email: " + email);
+      );
+
+    // Conver our User (domain) into User Spring Security
+    // Leave Roles empty for now
+    return new User(
+      ourUser.getEmail(),
+      ourUser.getPassword(),
+      Collection.emptyList() // Roles here ("ROLE_USER")
+    );
+
+  }
+  
+}
